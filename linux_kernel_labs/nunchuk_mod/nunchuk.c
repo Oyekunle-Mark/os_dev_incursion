58bc0ec9c5394950a44bc4468c25c0410422cc35
Oye Oloyede <oyekunlemac@gmail.com>
2024-08-25 21:45:40 +0100

    Added polling to the driver

--- linux_kernel_labs/nunchuk_mod/nunchuk.c
+++ linux_kernel_labs/nunchuk_mod/nunchuk.c
@@ -6,6 +6,9 @@
 #include <linux/input.h>
 
 static char buffer[6] = {0};
+struct nunchuk_dev {
+	struct i2c_client *i2c_client;
+};
 
 static void nunchuk_read_registers(struct i2c_client *client)
 {
@@ -16,7 +19,24 @@ static void nunchuk_read_registers(struct i2c_client *client)
 	usleep_range(10000, 20000);
 	int r = i2c_master_recv(client, buffer, 6);
 
-	pr_info("Read %d bytes from call to i2c_master_recv.\n", r);
+	if (r != 6) {
+		pr_alert("Did not receive up to 6 bytes.\n");
+	}
+}
+
+static void nunchuk_poll(struct input_dev *input)
+{
+	struct nunchuk_dev *nunchuk = input_get_drvdata(input);
+	struct i2c_client *client = nunchuk->i2c_client;
+	nunchuk_read_registers(client);
+
+	const char last_byte = buffer[5];
+	int zpressed = (last_byte & (0b00000001)) ? 0 : 1; // button Z is bit 0(lsb). 0 is pressed, 1 is release
+	int cpressed = ((last_byte >> 1) & (0b0000001)) ? 0 : 1; // button C is bit 1(from lsb). 0 is pressed, 1 is release
+
+	input_report_key(input, BTN_Z, zpressed);
+	input_report_key(input, BTN_C, cpressed);
+	input_sync(input);
 }
 
 static int nunchuk_probe(struct i2c_client *client)
@@ -36,23 +56,31 @@ static int nunchuk_probe(struct i2c_client *client)
 		return 1;
 	}
 
-	nunchuk_read_registers(client);	
-	nunchuk_read_registers(client);	
+	struct nunchuk_dev *nunchuk = devm_kzalloc(&client->dev, sizeof(*nunchuk), GFP_KERNEL);
 
-	const char last_byte = buffer[5];
-	int zpressed = last_byte & (0b00000001); // button Z is bit 0(lsb). 0 is pressed, 1 is release
-	int cpressed = (last_byte >> 1) & (0b0000001); // button C is bit 1(from lsb). 0 is pressed, 1 is release
+	if (!nunchuk)
+		return -ENOMEM;
 
-	pr_info("Z pressed: %d\n", zpressed);
-	pr_info("C pressed: %d\n", cpressed);
+	nunchuk->i2c_client = client;
+	i2c_set_clientdata(client, nunchuk);
 
 	struct input_dev *input = devm_input_allocate_device(&client->dev);
 	input->name = "Wii Nunchuk";
 	input->id.bustype = BUS_I2C;
+	input_set_drvdata(input, nunchuk);
 
 	set_bit(EV_KEY, input->evbit);
 	set_bit(BTN_C, input->keybit);
 	set_bit(BTN_Z, input->keybit);
+
+	int poll_ret = input_setup_polling(input, nunchuk_poll);
+
+	if (poll_ret) {
+		pr_alert("Failed to setup polling.\n");
+		return poll_ret;
+	}
+
+	input_set_poll_interval(input, 50);
 
 	int ret = input_register_device(input);
 
