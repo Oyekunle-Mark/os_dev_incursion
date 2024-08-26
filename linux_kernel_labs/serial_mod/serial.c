576b3f4bf9a956013bd60851d08b4b1f8dcf4021
Oye Oloyede <oyekunlemac@gmail.com>
2024-08-26 01:02:42 +0100

    Created the serial folder

---
+++ linux_kernel_labs/serial_mod/serial.c
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+
+static int serial_probe(struct platform_device *pdev)
+{
+	pr_info("Called %s\n", __func__);
+	return 0;
+}
+
+static int serial_remove(struct platform_device *pdev)
+{
+	pr_info("Called %s\n", __func__);
+	return 0;
+}
+
+static const struct of_device_id serial_of_match[] = {
+	{ .compatible = "bootlin,serial" },
+	{ },
+};
+MODULE_DEVICE_TABLE(of, serial_of_match);
+
+static struct platform_driver serial_driver = {
+        .driver = {
+                .name = "serial",
+                .owner = THIS_MODULE,
+				.of_match_table = of_match_ptr(serial_of_match),
+        },
+        .probe = serial_probe,
+        .remove = serial_remove,
+};
+module_platform_driver(serial_driver);
+
+MODULE_LICENSE("GPL");
