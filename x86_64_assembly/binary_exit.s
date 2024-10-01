1476baba06772824c799bba34ad8d22fb9d0d471
Oye Oloyede <oyekunlemac@gmail.com>
2024-10-01 09:40:01 +0100

    The binary exit assembly program

---
+++ x86_64_assembly/binary_exit.s
@@ -0,0 +1,8 @@
+.section .text
+.globl _start
+_start:
+	# move binary 1101(decimal 13) into rdi as status code of exit syscall
+	movq $0b1101, %rdi
+	# makk exit syscall so the decimal value of %rdi gets called
+	movq $60, %rax
+	syscall
