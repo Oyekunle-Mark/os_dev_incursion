ebb3a10debf6cae3f0bca2c0250ba026ba784f5d
Oye Oloyede <oyekunlemac@gmail.com>
2024-10-03 21:13:09 +0100

    commented the program

--- x86_64_assembly/exponent_loop.s
+++ x86_64_assembly/exponent_loop.s
@@ -7,12 +7,19 @@ _start:
 	movq $3, %rcx
 	# start accumulator at 1
 	movq $1, %rax
+
+	# if exponent is zero, we can wrap up the program
 	cmpq $0, %rcx
+	# jump if result of last comparison set the zero flag to 0
 	je comple
 mainloop:
+	# multiple %rax with %rbx
 	mulq %rbx
+	# the loopq command decrements %rcx(counter register)
+	# and jumps to the label if %rcx is zero(as a result of the decrement operation)
 	loopq mainloop
 complete:
+	# use exit syscall to view result
 	movq %rax, %rdi
 	movq $60, %rax
 	syscall
