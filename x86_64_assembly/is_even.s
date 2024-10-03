9607fa5a77ba471171781254d2629f2d196e0469
Oye Oloyede <oyekunlemac@gmail.com>
2024-10-03 21:39:21 +0100

    commented the code

--- x86_64_assembly/is_even.s
+++ x86_64_assembly/is_even.s
@@ -3,14 +3,22 @@
 .section .text
 .globl _start
 _start:
+	# start with number in %rax
 	movq $34357, %rax
+	# move divisor(divide by 2 to find if even) to %rsi
+	# divq only accepts a source that is a register or memory location
 	movq $2, %rsi
+	# divide
 	divq %rsi
 
+	# is the remainder 0?
 	cmpq $0, %rdx
+	# jump to even label to set exit code
 	je even
 
+	# number is odd, set exit code
 	movq $0, %rdi
+	# jump to exit label to issue exit syscall
 	jmp exit
 even:
 	movq $1, %rdi 
