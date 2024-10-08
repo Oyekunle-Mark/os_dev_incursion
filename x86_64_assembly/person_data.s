264a5847245b9793766d46539cd529ced13db3ed
Oye Oloyede <oyekunlemac@gmail.com>
2024-10-08 21:06:03 +0100

    modified the person data file and added the shoe field to the person record

--- x86_64_assembly/person_data.s
+++ x86_64_assembly/person_data.s
@@ -9,23 +9,26 @@ num_people:
 # person structure as array of quad words
 # person structures arranged in succession in memory
 people:
-	.quad 200, 2, 74, 20
-	.quad 280, 2, 72, 44
-	.quad 150, 1, 68, 30
-	.quad 250, 3, 75, 24
-	.quad 250, 2, 70, 11
-	.quad 180, 5, 69, 65
+	# array of people
+	.quad 200, 10, 2, 74, 20
+	.quad 280, 12, 2, 72, 44
+	.quad 150, 8, 1, 68, 30
+	.quad 250, 14, 3, 75, 24
+	.quad 250, 10, 2, 70, 11
+	.quad 180, 11, 5, 69, 65
 # end_people marks the end of the person structures
 # used to find size of all person records
 end_people:
 
 # globals used for person record(structure) field access
-.globl WEIGHT_OFFSET, HAIR_OFFSET, HEIGHT_OFFSET, AGE_OFFSET
+.globl WEIGHT_OFFSET, SHOE_OFFSET, HAIR_OFFSET, HEIGHT_OFFSET, AGE_OFFSET
 # constants for offset of fields in the person record
 .equ WEIGHT_OFFSET, 0
-.equ HAIR_OFFSET, WEIGHT_OFFSET + 8
+.equ SHOE_OFFSET, WEIGHT_OFFSET + 8
+.equ HAIR_OFFSET, SHOE_OFFSET + 8
 .equ HEIGHT_OFFSET, HAIR_OFFSET + 8
 .equ AGE_OFFSET, HEIGHT_OFFSET + 8
+
 # the size of the person record
 .globl PERSON_RECORD_SIZE
-.equ PERSON_RECORD_SIZE, 32
+.equ PERSON_RECORD_SIZE, 40
