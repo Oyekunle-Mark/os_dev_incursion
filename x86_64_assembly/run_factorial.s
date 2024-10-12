574a21f405d6e06993ca100d56bdb6a29f56cf51
Oye Oloyede <oyekunlemac@gmail.com>
2024-10-12 23:09:27 +0100

    commented the program

--- x86_64_assembly/run_factorial.s
+++ x86_64_assembly/run_factorial.s
@@ -1,9 +1,12 @@
 .section .text
 .globl _start
 _start:
+	# parameter to factorial is 4
 	movq $4, %rdi
 	call factorial
 
+	# the value returned by factorial is passed as
+	# the exit code to the exit syscall
 	movq %rax, %rdi
 	movq $60, %rax
 	syscall
