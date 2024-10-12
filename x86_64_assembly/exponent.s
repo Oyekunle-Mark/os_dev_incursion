1a8e4385b21c82a52d403e48d1d979c4dccf2cfa
Oye Oloyede <oyekunlemac@gmail.com>
2024-10-12 22:31:53 +0100

    added comments to the code

--- x86_64_assembly/exponent.s
+++ x86_64_assembly/exponent.s
@@ -6,13 +6,36 @@
 
 .section .text
 exponent:
+	# base is in %rdi
+	# exponent is in %rsi
+
+	# issue enter to create a new stack frame
+	# enter $16, $0 is equivalent to:
+	# 1. pushq %rbp
+	# 2. movq %rsp, %rbp
+	# 3, subq $16, %rsp
 	enter $16, $0
+	# set the accumulator to 1
 	movq $1, %rax
+	# move the exponent into the stack
+	# %rbp is used for referencing the stack
 	movq %rsi, -8(%rbp)
 main_loop:
+	# multiply the base by %rax
 	mulq %rdi
+	# decrement the exponent
 	decq -8(%rbp)
+	# jump to repeat multiplication if expoent is
+	# greater than zero
 	jnz mainloop
 complete:
+	# issue leave to destroy the stack frame
+	# leave is equivalent to:
+	# 1. movq %rbp, %rsp
+	# 2. popq %rbp
 	leave
+	# ret returns from the function
+	# ret is equivalent to:
+	# 1. popq %rip(pops the return address from the stop of the stack)
+	# 2. jmp <to address popped in 1 above>
 	ret
